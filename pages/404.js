559c82918457c72e045299f19156fb7fb90ac33c
unknown <yosshimendieta94@gmail.com>
2022-06-02 11:27:58 -0500

    Sistema Terminado V1

--- pages/404.js
+++ pages/404.js
@@ -16,7 +16,7 @@ const NotFound = () => {
       <Divider my={6} />
       <Box my={6} align="center">
         <NextLink href="/">
-          <Button colorScheme="teal">Return to home</Button>
+          <Button colorScheme="teal">Return to Home</Button>
         </NextLink>
       </Box>
     </Container>
